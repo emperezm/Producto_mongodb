f6bf15a3208f2acd88f3d3cc6525b3f93af5b5f9
Mariana Perez <mari-perez36@hotmail.com>
2025-04-08 22:46:52 -0500

    Se asigna el proceso de conexión sobre la base de datos local de MongoDB

--- database.py
+++ database.py
@@ -1,30 +1,18 @@
-#from pymongo import MongoClient
+from pymongo import MongoClient
 
 # URL de conexión para MongoDB en tu PC (localhost en el puerto 27017)
-#MONGO_URI = "mongodb://localhost:27017/"
+MONGO_URI = "mongodb://localhost:27017/"
 
-#def dbConnection():
-    #try:
-        # Crear la conexión con MongoDB
-         #client = MongoClient(MONGO_URI)
-        #db = client["dbb_producto"]  # Nombre de tu base de datos
-        #print("✅ Conexión exitosa a MongoDB")
-        #return db
-    #except Exception as e:
-        #print(f"❌ Error al conectar a MongoDB: {e}")
-        #return None
-
-import pyodbc
-
-# Configurar la conexión a la base de datos SQL Server
 def dbConnection():
-    conn = pyodbc.connect(
-        "DRIVER={SQL Server};"
-        "SERVER=localhost\\SQLEXPRESS;"  # Nombre del servidor correcto
-        "DATABASE=Productos;"  # Nombre de la base de datos correcto
-        "Trusted_Connection=yes;"  # Conexión segura sin usuario/contraseña
-    )
-    return conn
+    try:
+        # Crear la conexión con MongoDB
+        client = MongoClient(MONGO_URI)
+        db = client["dbb_producto"]  # Nombre de tu base de datos
+        print("✅ Conexión exitosa a MongoDB")
+        return db
+    except Exception as e:
+        print(f"❌ Error al conectar a MongoDB: {e}")
+        return None
 
 # Comprobar conexión
 if __name__ == '__main__':
